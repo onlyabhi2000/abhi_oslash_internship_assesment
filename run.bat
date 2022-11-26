241b6d5f9d3d5c1877132cdd7444d8d8305b6017
onlyabhi2000 <103091157+onlyabhi2000@users.noreply.github.com>
2022-11-26 23:20:46 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+
+python -m oslash_abhi sample_input\input1.txt
+python -m oslash_abhi sample_input\input2.txt
+python -m oslash_abhi sample_input\input3.txt
+python -m oslash_abhi sample_input\input4.txt
